a31294d2cc185ea783e3d6a1ed1afbcc3e83efdd
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-12-08 11:20:46 +0100

    update script and readme

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 > Template for starting development with [Swift](https://swift.org) and [Vapor Web Framework](https://vapor.codes) on Vagrant's virtual machine powered by [Ubuntu 16.04](https://atlas.hashicorp.com/ubuntu/boxes/xenial64).
 
+## Usage
+
+```bash
+git clone git@github.com:voronianski/swift-vapor-vagrant.git
+cd swift-vapor-vagrant
+rm -rf .git
+vagrant up
+vagrant ssh
+```
+
+## What's inside?
+
+- [Ubuntu 16.04](https://atlas.hashicorp.com/ubuntu/boxes/xenial64)
+- [Swift 3.0.1](https://swift.org)
+- [Vapor Framework Toolbox](https://vapor.codes)
+
 ---
 
 **MIT Licensed**
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-OS="ubuntu1604"
 TC="swift-3.0.1-RELEASE-ubuntu16.04"
 
 echo "📦  Installing Dependencies"
@@ -10,22 +9,24 @@ sudo apt-get update --yes
 # libcurl3 - http://stackoverflow.com/questions/39812998/swift-build-error-on-ubuntu-15-10
 sudo apt-get -yq install clang libicu-dev git wget libpython2.7-dev libcurl3
 
-echo "⬇️  Downloading"
-wget https://swift.org/builds/swift-3.0.1-release/$OS/swift-3.0.1-RELEASE/$TC.tar.gz
+echo "⬇️  Downloading Swift"
+wget -q https://swift.org/builds/swift-3.0.1-release/ubuntu1604/swift-3.0.1-RELEASE/$TC.tar.gz
 
-echo "🔧  Untarring"
+echo "🔧  Untarring Swift"
 tar zxf $TC.tar.gz
 rm -rf $TC.tar.gz
 
-echo "🚀  Installing"
+echo "🚀  Installing Swift"
 sudo mv $TC /swift-3.0.1
 
-echo "export PATH=/swift-3.0.1/usr/bin:$PATH" >> ~/.bashrc
-exec bash
+echo "export PATH=/swift-3.0.1/usr/bin:\${PATH}" >> ~/.bashrc
+source .bashrc
 
+echo "🔄  Checking Swift"
 curl -sL check.vapor.sh | sudo bash
-curl -sL toolbox.vapor.sh | sudo bash
 
+echo "💧  Installing Vapor Toolbox"
+curl -sL toolbox.vapor.sh | sudo bash
 vapor --help
 
 echo "✅  Done"
